22c3e2392752f1375308fd9caac7b0dc6cb6008d
nirmal6353 <58050332+nirmal6353@users.noreply.github.com>
2019-11-26 21:29:26 +0530

    Add files via upload
    yuyuygug

---
+++ proj23.cpp
@@ -0,0 +1,673 @@
+#include<bits/stdc++.h>
+#include <conio.h>
+
+char disp_name[100],gen;
+
+
+
+
+using namespace std;
+class faculty{
+	private:
+		char user[100];
+	    char password[100];
+    public:
+        class Leave{
+        public:
+            int cl,spl,vl,el;
+            char from_date[11];
+            char to_date[11];
+        };
+        Leave f_leave;
+        
+        
+        
+        int count=5;
+        
+        char mail_id[50],response[25];
+        faculty(){
+        	while(count--) {
+			cout<<"      --------- "<<endl;
+            cout<<"       SIGN IN  "<<endl;
+            cout<<"      --------- "<<endl;
+            cout<<"Username : ";
+            cin>>user;
+            cout<<"Password : ";
+            cin.ignore();
+            cin>>password;
+           /* int i=0;
+            char ch;
+            
+   while (1) {
+      if (i < 0) {
+         i = 0;
+      }
+      ch = getch();
+ 
+      if (ch == 13)
+         break;
+      if (ch == 8) /*ASCII value of BACKSPACE
+      {
+      	if(i!=0)
+      	{
+		
+        putch('\b');
+		putch(NULL);
+		putch('\b');
+		if(i>0)
+		password[i-1]='\0'; 
+         i--;}
+         continue;
+      }
+ 
+      password[i++] = ch;
+      ch = '*';
+      putch(ch);
+   }*/
+            FILE *fp=fopen("login2.txt","r");
+            char user_1[100],password_1[100];
+            int flag=0;
+            
+          while(fscanf(fp,"%s %s %s %c",user_1,password_1,disp_name,&gen)!=EOF)
+          {
+            
+            if(strcmp(user,user_1)==0&&strcmp(password,password_1)==0)
+            {
+                flag=1;
+          
+                system("CLS");
+                cout<<endl<<"LOGIN SUCCESSFUL"<<endl<<endl;
+                fclose(fp);
+                break;
+            }    
+         }
+         if(flag==1)
+         break;
+         else
+         {
+         	system("CLS");
+		 if(count!=0)
+		   cout<<endl<<"         AUNTHENTICATION FAILED"<<endl<<endl<<"*Please enter valid login credentials "<<endl<<"*You have "<<count<<" chances remaining "<<endl<<endl;
+            if(count==0)
+         {
+			   cout<<endl<<"*You have reached the maximum count"<<endl<<endl<<"                 Please try again later"<<endl;
+           fclose(fp);
+			exit(1);}
+         
+         	
+	
+	}
+	
+         }
+         
+        
+        
+}}; 
+
+
+
+
+class HOD
+{
+		private:
+		char user[100];
+	    char password[100];
+    public:
+        
+        int count=5;
+            char user_1[100],password_1[100],bran[4];
+        
+        char mail_id[50],response[25];
+        HOD(){
+        	while(count--) {
+			cout<<"      --------- "<<endl;
+            cout<<"       SIGN IN  "<<endl;
+            cout<<"      --------- "<<endl;
+            cout<<"Username : ";
+            cin>>user;
+            cout<<"Password : ";
+            cin.ignore();
+            cin>>password;
+           /* int i=0;
+            char ch;
+            
+   while (1) {
+      if (i < 0) {
+         i = 0;
+      }
+      ch = getch();
+ 
+      if (ch == 13)
+         break;
+      if (ch == 8) /*ASCII value of BACKSPACE
+      {
+      	if(i!=0)
+      	{
+		
+        putch('\b');
+		putch(NULL);
+		putch('\b');
+		if(i>0)
+		password[i-1]='\0'; 
+         i--;}
+         continue;
+      }
+ 
+      password[i++] = ch;
+      ch = '*';
+      putch(ch);
+   }*/
+            FILE *fp=fopen("HODF.txt","r");
+            
+            int flag=0;
+            
+          while(fscanf(fp,"%s %s %s ",user_1,password_1,bran)!=EOF)
+          {
+            
+            if(strcmp(user,user_1)==0&&strcmp(password,password_1)==0)
+            {
+                flag=1;
+          
+                system("CLS");
+                cout<<endl<<"LOGIN SUCCESSFUL"<<endl<<endl;
+                fclose(fp);
+                
+                break;
+            }    
+         }
+         if(flag==1)
+         break;
+         else
+         {
+         	system("CLS");
+		 if(count!=0)
+		   cout<<endl<<"         AUNTHENTICATION FAILED"<<endl<<endl<<"*Please enter valid login credentials "<<endl<<"*You have "<<count<<" chances remaining "<<endl<<endl;
+            if(count==0)
+         {
+			   cout<<endl<<"*You have reached the maximum count"<<endl<<endl<<"                 Please try again later"<<endl;
+           fclose(fp);
+			exit(1);}
+		}
+         
+   }}
+	
+};
+
+class Admin
+{
+private:
+	char user[100];
+	char password[100];
+	public:
+		Admin()
+		{
+				strcpy(user,"admin");
+		strcpy(password,"xxx");
+			
+			int count=5,flag=0;
+		char user_1[100],password_1[100];
+		while(count--&&flag==0)
+		{
+			cout<<"ENTER THE USER NAME"<<endl;
+			cin>>user_1;
+			cout<<"ENTER THE PASSWORD"<<endl;
+			cin>>password_1;
+			if(strcmp(user,user_1)==0&&strcmp(password,password_1)==0)
+			{
+				cout<<"login successful"<<endl;
+				flag=1;
+				break;
+			}
+			else
+			cout<<"AUTHENTICATION FAILED TRY AGAIN"<<endl;
+		}
+}};
+
+class Director
+{
+};
+
+int main()
+{
+	
+	cout<<"Login as:\nA)Faculty\nB)HOD\nC)Admin\nD)Director\n\nEnter option: ";
+	char log_opt;
+	char opt;
+	int n_days;
+	char dep[4];
+	cin>>log_opt;
+	char name[100];
+	char str='E';
+	
+	
+	if(log_opt=='A')
+    
+	{
+	faculty fac_1;
+	
+	cout<<"What do you want to do?\nA)Apply Leave\nB)Check Notification\n\nenter option: ";
+	char op_ab;
+	cin>>op_ab;
+	
+	system("CLS");
+	
+    if(op_ab=='A') {
+	
+    
+	cout<<"                                         ________________________________________       "<<endl;
+    cout<<"                                                                                    "<<endl;
+	cout<<"                                           IITJ FACULTY LEAVE MANAGEMENT SYSTEM        "<<endl;
+	cout<<"                                         ________________________________________        "<<endl<<endl;
+		
+	
+	int flag_2=0;
+	
+	if(gen=='M')
+	cout<<"Welcome Mr."<<disp_name<<" ,"<<endl<<endl;
+	if(gen=='F')
+	cout<<"Welcome Ms."<<disp_name<<" ,"<<endl<<endl;	
+	
+	int k=1;
+	
+	cout<<k++<<") Enter your full name: ";
+	cin.ignore();
+	
+	gets(name);
+	
+	
+	cout<<endl<<k++<<") Department (CSE/EE/BB/ME) : ";
+	cin>>dep;
+	
+	cout<<endl<<k++<<") Select the type of leave:-"<<endl<<endl<<"    A. Casual Leave"<<endl<<"    B. Special Casual Leave"<<endl<<"    C. Vacation Leave"<<endl<<"    D. Earn Leave"<<endl<<endl;
+	while(flag_2==0)
+	{
+	cout<<"    -> Enter option: ";
+	cin>>opt;
+	
+
+	
+
+	switch(opt)
+	{
+	   case 'A':
+	
+	     	fac_1.f_leave.cl=n_days;
+	     	cout<<endl<<k++<<") Enter the number of days of leave: ";
+	        cin>>n_days;
+	     	flag_2=1;
+		    break;
+	
+	   case 'B':
+	
+	 	    fac_1.f_leave.spl=n_days;
+	 	    cout<<endl<<k++<<") Enter the number of days of leave: ";
+	        cin>>n_days;
+	 	    flag_2=1;
+	        break;
+	   case 'C':
+	
+	     	fac_1.f_leave.vl=n_days;
+	        cout<<endl<<k++<<") Enter the number of days of leave: ";
+	        cin>>n_days;
+	     	flag_2=1;
+	        break;
+	   case 'D':
+	
+		    fac_1.f_leave.el=n_days;
+		   	cout<<endl<<k++<<") Enter the number of days of leave: ";
+	        cin>>n_days;
+		    flag_2=1;
+		    break;
+	   default:
+	   	
+	   	    cout<<"Please enter valid option : ";
+	   	    break;
+}
+if(flag_2==1)
+break;
+}
+	
+
+//	char opt;
+	cout<<endl<<k++<<") Enter date of leave (dd/mm/yyyy) : ";
+	cin>>fac_1.f_leave.from_date;
+	cout<<endl<<k++<<") Enter date of arrival (dd/mm/yyyy) : ";
+	cin>>fac_1.f_leave.to_date;
+	cin.ignore();
+	
+	cout<<endl<<k++<<") Administrative responsibility assigned to : ";
+	gets(fac_1.response);
+	FILE *fp;
+	
+	
+	fp=fopen("responsibility2.txt","a");
+	
+	
+
+	fprintf(fp,"%s %s %c %d %s %s %s %c\n",name,dep,opt,n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,str);
+      fclose(fp);
+	
+}
+
+if(op_ab=='B')
+{
+
+	cout<<"Enter your full name: ";
+	char resp_nam[50];
+	cin>>resp_nam;
+	
+	FILE *fp;
+	
+	fp=fopen("responsibility2.txt","r+");
+	
+	fseek(fp,1,SEEK_CUR);
+	while(fscanf(fp,"%s %s %c %d %s %s %s %c",name,dep,&opt,&n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,&str)!=EOF)
+	{
+		
+		cin.ignore();
+		cout<<"1"<<endl;
+		cout<<fac_1.response;
+		
+		if(strcmp(fac_1.response,resp_nam)==0 && (str=='E'))
+		{
+			//cout<<"1"<<endl;
+			cout<<name<<endl<<dep<<endl<<fac_1.f_leave.from_date<<endl<<fac_1.f_leave.to_date<<endl;
+			cout<<endl<<"Do you want to take the responsibility?\n Y/N  ";
+			char y_n;
+			cin>>y_n;
+			
+			if(y_n=='Y')
+			{
+			 str='Y';
+			
+			
+			if(strcmp(dep,"CSE")==0)
+			{
+				char status='E';
+				FILE *fs;
+				fs=fopen("HODCS.txt","a");
+				
+				fprintf(fs,"%s %s %c %d %s %s %s %c\n",name,dep,opt,n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,status);
+				fclose(fs);
+			}
+		   else	if(strcmp(dep,"EE")==0)
+			{
+				FILE *fs;
+				fs=fopen("HODEE.txt","a");
+				char status='E';
+				fprintf(fs,"%s %s %c %d %s %s %s %c\n",name,dep,opt,n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,status);
+				fclose(fs);
+			}
+			else if(strcmp(dep,"ME")==0)
+			{
+				FILE *fs;
+				fs=fopen("HODME.txt","a");
+				char status='E';
+				fprintf(fs,"%s %s %c %d %s %s %s %c\n",name,dep,opt,n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,status);
+				fclose(fs);
+			}
+			else if(strcmp(dep,"BB")==0)
+			{
+				FILE *fs;
+				fs=fopen("HODBB.txt","a");
+				char status='E';
+				fprintf(fs,"%s %s %c %d %s %s %s %c\n",name,dep,opt,n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,status);
+				fclose(fs);
+			}
+		}
+			else
+			{
+		      str='N';
+			}
+			
+			fseek(fp,-1,SEEK_CUR);
+			
+			fputc(str,fp);
+			fseek(fp,1,SEEK_CUR);
+		}
+	}
+		rewind(fp);
+		fseek(fp,1,SEEK_CUR);
+		while(fscanf(fp,"%s %s %c %d %s %s %s %c",name,dep,&opt,&n_days,fac_1.f_leave.from_date,fac_1.f_leave.to_date,fac_1.response,&str)!=EOF)
+		{
+			if(strcmp(resp_nam,name)==0)
+			{
+				if(str=='Y')
+				{
+				cout<<"YOUR ACADEMIC RESPONSIBLITY IS ACCEPTED BY "<<fac_1.response;
+				fseek(fp,-1,SEEK_CUR);
+				fputc('C',fp);
+				}
+				if(str=='N')
+				{
+				cout<<"YOUR ACADEMIC RESPONSIBLITY IS REJECTED BY "<<fac_1.response;
+				fseek(fp,-1,SEEK_CUR);
+				fputc('C',fp);
+				}
+			}
+			fseek(fp,1,SEEK_CUR);
+		}
+		
+		
+	
+	
+	fclose(fp);
+	
+}
+
+}
+
+else if(log_opt=='B')
+{
+	HOD hd;
+	char Name[50],FROM[50],TO[50],DEP[4],RESP[50],STR;
+	int nn_days;
+	char oopt;
+	
+	if(strcmp(hd.bran,"CSE")==0)
+	{
+		FILE *fp;
+		//cout<<"1"<<endl;
+	
+	fp=fopen("HODCS.txt","r+");
+	
+	fseek(fp,1,SEEK_CUR);
+	while(fscanf(fp,"%s %s %c %d %s %s %s %c",Name,DEP,&oopt,&nn_days,FROM,TO,RESP,&STR)!=EOF)
+	{
+		
+		//cin.ignore();
+		
+		if(STR=='E')
+		{
+			
+			cout<<Name<<endl<<DEP<<endl<<FROM<<endl<<TO<<endl;
+			cout<<endl<<"Do you want to take the responsibility?\n Y/N  ";
+			char y_n;
+			cin>>y_n;
+			
+			if(y_n=='Y')
+			{
+			 STR='Y';
+			 cout<<"1"<<endl;
+			 char status='E';
+			 
+			 FILE *fs;
+			 char dirst='E';
+			 fs=fopen("ADMIN.txt","a");
+			 fprintf(fs,"%s %s %c %d %s %s %s %c %c\n",Name,DEP,oopt,nn_days,FROM,TO,RESP,status,dirst);
+			 fclose(fs);
+		}
+		else
+		{
+			STR='N';
+		}
+		
+			fseek(fp,-1,SEEK_CUR);
+			
+			fputc(STR,fp);
+			fseek(fp,1,SEEK_CUR);
+			
+			
+		
+		
+	}
+	
+}
+
+fclose(fp);
+}
+	
+	
+	
+	
+	
+}
+
+else if(log_opt=='C')
+{
+	Admin ad;
+    char nname[50],depp[4],optt,frrom[50],too[50],rresp[50];
+	int no_days;
+	char stt;	
+	char fname[50];
+	int ccl,sccl,vvl,ele;
+	FILE *fp;
+	char dirst2;
+	fp=fopen("ADMIN.txt","r+");
+	fseek(fp,1,SEEK_CUR);
+	while(fscanf(fp,"%s %s %c %d %s %s %s %c %c",nname,depp,&optt,&no_days,frrom,too,rresp,&stt,&dirst2)!=EOF)
+	{
+		if(stt=='E')
+		{
+			FILE *fs;
+			fs=fopen("datasheet.txt","r+");
+			while(fscanf(fs,"%s %d %d %d %d",fname,&ccl,&sccl,&vvl,&ele)!=EOF)
+			{
+				if(strcmp(fname,nname)==0)
+				{
+					if(optt=='A')
+					{
+						if(ccl<no_days)
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('N',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+						else
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('Y',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+					}
+					if(optt=='B')
+					{
+							if(sccl<no_days)
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('N',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+						else
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('Y',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+						
+					}
+					if(optt=='C')
+					{
+							if(vvl<no_days)
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('N',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+						else
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('Y',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+					}
+					if(optt=='D')
+					{
+							if(ele<no_days)
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('N',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+						else
+						{
+							fseek(fp,-3,SEEK_CUR);
+							fputc('Y',fp);
+							fseek(fp,3,SEEK_CUR);
+						}
+					}
+				}
+			}
+			fclose(fs);
+		}
+		
+		else if(dirst2=='Y')
+		{
+			struct emp
+			{
+				char ffname[50];
+				int clc,sclc,vlv,eel;
+			}e;
+			FILE *fs;
+			fs=fopen("datasheet.txt","rb+");
+			while(fread(&e,sizeof(e),1,fp)==1)
+			{
+				if(strcmp(e.ffname,nname)==0)
+				{
+					int len=strlen(nname);
+			    	if(optt=='A')
+					{
+						e.clc=e.clc-no_days;
+						fseek(fs,-(len+9),SEEK_CUR);
+				        fwrite(&e,sizeof(e),1,fs);
+					    //fseek(fs,1,SEEK_CUR); 
+				    }
+				    if(optt=='B')
+					{
+						fseek(fs,-(len+9),SEEK_CUR);
+				        fprintf(fs,"%s %d %d %d",fname,ccl,sccl-no_days,vvl,ele);
+					    fseek(fs,1,SEEK_CUR); 
+				    }
+				    if(optt=='C')
+					{
+						fseek(fs,-(len+9),SEEK_CUR);
+				        fprintf(fs,"%s %d %d %d",fname,ccl,sccl,vvl-no_days,ele);
+					    fseek(fs,1,SEEK_CUR);
+				    }
+				    if(optt=='D')
+					{
+						fseek(fs,-(len+9),SEEK_CUR);
+				        fprintf(fs,"%s %d %d %d",fname,ccl,sccl,vvl,ele-no_days);
+					    fseek(fs,1,SEEK_CUR);
+				    }
+			    }
+		    }
+		    fclose(fs);
+	    }
+}
+	
+	fclose(fp);
+	
+}
+
+else if(log_opt=='D')
+{
+	Director dd;
+	
+}
+	
+	
+	
+	
+	return(0);
+}
